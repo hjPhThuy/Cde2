a2a3e34eb44a54670e68dbd9a0fee52fdfa389d0
hjPhThuy <phuong.thuy20122003@gmail.com>
2025-10-31 13:47:25 +0700

    setup clerk

--- server/server.js
+++ server/server.js
@@ -2,11 +2,17 @@ import express from 'express'
 import "dotenv/config";
 import cors from "cors";
 import connectDB from './configs/db.js';
-
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from './controllers/clerkWebhooks.js';
 connectDB()
 
 const app = express()
 app.use(cors()) 
+
+app.use(express.json())
+app.use(clerkMiddleware())
+
+app.use("/api/clerk", clerkWebhooks);
 
 app.get('/', (req, res) => res.send("API đang hoạt động"))
 
